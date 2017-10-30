419e9d39a1aca444bd4e4c6625e883abe3199cf3
Ilya Gutnikov <ilyagutnikov93@gmail.com>
2017-10-30 22:20:13 +0300

    Сделал первичную структуру

--- unitydebug/Assets/Scripts/DebugPrefix.cs
+++ unitydebug/Assets/Scripts/DebugPrefix.cs
@@ -2,15 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DebugPrefix : MonoBehaviour {
+namespace UnityDebug.DebugPrefixes
+{
+    [CreateAssetMenu(fileName = "DebugPrefixes", menuName = "NewDebugPrefixes")]
+    public class DebugPrefix : ScriptableObject
+    {
+        public string[] DebugPrefixes;
+        
+        public DebugPrefix()
+        {
+            DebugPrefixes = new string[1];
+        }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        public List<string> PrefixesToList()
+        {
+            var list = new List<string>();
+
+            foreach (var prefix in DebugPrefixes)
+            {
+                list.Add(prefix);
+            }
+
+            return list;
+        }
+
+        public string[] PrefixesToArray(List<string> prefixesInList)
+        {
+            if (prefixesInList.Count == 0)
+            {
+                Debug.LogError("Лист префиксов пуст");
+            }
+
+            string[] array = new string[prefixesInList.Count];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = prefixesInList[i];
+            }
+
+            DebugPrefixes = array;
+
+            return array;
+        }
+
+    }
 }
